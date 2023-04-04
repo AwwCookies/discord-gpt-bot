d224fdb1b74b7d34c1f8527250dbebc62a7ebf21
AwwCookies <emma@emaw.dev>
2023-04-03 22:24:26 -0400

    hey github

---
+++ index.js
@@ -0,0 +1,54 @@
+require('dotenv/config')
+
+const { Client, IntentsBitField } = require('discord.js')
+const { Configuration, OpenAIApi } = require('openai')
+
+let conversationLog = [{role: 'system', content: "You are a professional developer"}]
+
+function resetConversation() {
+  conversationLog = [{role: 'system', content: "You are a professional developer"}]
+}
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent
+  ]
+})
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_KEY
+})
+const openai = new OpenAIApi(configuration)
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return
+  if (message.channelId !== process.env.CHANNEL_ID) return
+  
+  // Reset conversation if user types !reset
+  if (message.content.startsWith('!reset')) {
+    resetConversation()
+    return message.reply('Conversation reset')
+  }
+
+  // else add message to conversation log
+
+  conversationLog.push({role: 'user', content: message.content})
+
+  await message.channel.sendTyping()
+  const result = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: conversationLog,
+  })
+
+  const msg = result.data.choices[0].message ? result.data.choices[0].message : '`Error: No response from OpenAI`'
+
+  message.reply(msg)
+})
+
+client.login(process.env.DISCORD_TOKEN)
